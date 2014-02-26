d3198913de2e6ec64cac8662c97b5dfcf59242ed
Mr. Jenkins <jenkins@mobbuild.interwoven.com>
2014-02-26 10:02:26 -0600

    test commit

--- HelloJenkinsCI/MasterViewController.m
+++ HelloJenkinsCI/MasterViewController.m
@@ -5,7 +5,7 @@
 //  Created by Mr. Jenkins on 2/26/14.
 //  Copyright (c) 2014 Jenkins. All rights reserved.
 //
-
+//test change
 #import "MasterViewController.h"
 
 #import "DetailViewController.h"
