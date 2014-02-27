ea370bae232324e55c4ec6151b5c51ff80f4ffce
Jenkins <qauser.hpq@gmail.com>
2014-02-27 12:30:10 -0600

    test

--- HelloJenkinsCI/DetailViewController.m
+++ HelloJenkinsCI/DetailViewController.m
@@ -5,7 +5,7 @@
 //  Created by Mr. Jenkins on 2/26/14.
 //  Copyright (c) 2014 Jenkins. All rights reserved.
 //
-
+//test again
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
