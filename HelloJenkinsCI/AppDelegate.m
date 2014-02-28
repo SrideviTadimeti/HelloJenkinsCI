3a0314d6183345d9fafe0ef9fa7789f13e3efd51
Jenkins <qauser.hpq@gmail.com>
2014-02-28 16:04:51 -0600

    dsfasdf

--- HelloJenkinsCI/AppDelegate.m
+++ HelloJenkinsCI/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-//test checkin
+//test checkin123
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
