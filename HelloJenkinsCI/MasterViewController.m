719861539eff6891b5f6f750896828cb481eb4c5
SrideviTadimeti <sridevi.588@gmail.com>
2014-03-03 10:51:14 -0600

    sdfsdf

--- HelloJenkinsCI/MasterViewController.m
+++ HelloJenkinsCI/MasterViewController.m
@@ -5,7 +5,7 @@
 //  Created by Mr. Jenkins on 2/26/14.
 //  Copyright (c) 2014 Jenkins. All rights reserved.
 //
-//test change 2
+//test change 2 5
 #import "MasterViewController.h"
 
 #import "DetailViewController.h"
